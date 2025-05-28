8393f02b960f1870049f64977971ea389dcfddc3
Nina Preußler <nina.preussler@stud.leuphana.de>
2025-05-28 19:38:46 +0200

    added omexdia_n2o_nope and nope

--- src/hereon_model_library.F90
+++ src/hereon_model_library.F90
@@ -1,7 +1,7 @@
 !! SPDX-FileCopyrightText: 2022-2025 Helmholtz-Zentrum hereon GmbH
 !! SPDX-License-Identifier: CC0-1.0
 !! SPDX-FileContributor Carsten Lemmen <carsten.lemmen@hereon.de>
-!! SPDX-FileContributor Nina Preußer <nina.preusser@stud.leuphana.de>
+!! SPDX-FileContributor Nina Preußler <nina.preussler@stud.leuphana.de>
 
 module hereon_model_library
 
@@ -26,6 +26,8 @@ contains
       use hereon_omexdia_p
       use hereon_omexdia_bottom
       use hereon_omexdia_n2o
+      use hereon_omexdia_n2o_nope
+      use hereon_nope
 
       class (type_factory), intent(in) :: self
       character(*),         intent(in) :: name
@@ -36,6 +38,8 @@ contains
          case ('omexdia_p'); allocate(type_hereon_omexdia_p::model)
          case ('omexdia_bottom'); allocate(type_hereon_omexdia_bottom::model)
          case ('omexdia_n2o'); allocate(type_hereon_omexdia_n2o::model)
+         case ('omexdia_n2o_nope'); allocate(type_hereon_omexdia_n2o_nope::model)
+         case ('nope'); allocate(type_hereon_nope::model)
       end select
 
    end subroutine
